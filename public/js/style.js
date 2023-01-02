d0fd99014cc6bd8215beb223609393820e6c109e
sahitikappagantula <gaganvishwakarma46@gmail.com>
2023-01-02 21:45:19 +0530

    Third Commit

---
+++ public/js/style.js
@@ -0,0 +1,8 @@
+let heading = document.getElementById("heading");
+let h2 = document.getElementById("h2")
+window.onload = function removeheight() {
+    heading.style.height = "46vh";  
+    h2.style.height = "46vh";
+    h2.style.marginBottom = "0rem";
+    heading.style.marginBottom = "0rem";
+}
--- src/app.js
+++ src/app.js
@@ -1,21 +1,31 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
 const port = process.env.port || 7000;
-
-const staticpath = path.join(__dirname, '../public');
 
 app.set("views", "../views");
 app.set("view engine", "ejs");
 
-app.use(express.static(staticpath));
+app.use(express.static(path.join(__dirname, '../public')));
 
+app.use('/assests', express.static('../public'));
 
 require("./db/conn")
 
-app.get("", (req, res) => {
+app.get("/", (req, res) => {
     res.render("index");
+})
+app.get("/index.ejs", (req, res) => {
+    res.render("index.ejs");
+})
+app.get("/shift1.ejs", (req, res) => {
+    res.render("shift1.ejs");
+})
+app.get("/shift2.ejs", (req, res) => {
+    res.render("shift2.ejs");
+})
+app.get("/about.ejs", (req,res) => {
+    res.render("about.ejs");
 })
 
 app.listen(port, () => {
