736695a248eb29d8f997a9934e187d5a868f3ad6
sahitikappagantula <gaganvishwakarma46@gmail.com>
2023-01-13 22:42:39 +0530

    tenth

--- src/index.js
+++ src/index.js
@@ -31,88 +31,13 @@ router.get("/admin_shift1.ejs", (req, res) => {
                 console.log(err);
             } else {
                 res.render("admin_shift1.ejs", {
-                    data: result
+                    data: result,
+                    action: 'add'
                 })
             }
         })
     })
 })
-router.post("/admin_shift1.ejs", function (request, response, next) {
-
-    var action = request.body.action;
-    if (action == 'Add') {
-        var route_no = request.body.route_no;
-
-        var bus_no = request.body.bus_no;
-
-        var address = request.body.address;
-
-        var query = `
-		INSERT INTO shift_1 
-		(route_no, bus_no, address) 
-		VALUES ("${route_no}", "${bus_no}", "${address}")
-		`;
-
-        database.query(query, function (error, data) {
-
-            response.json({
-                message: 'Data Added'
-            });
-
-        });
-    }
-
-    if (action == 'fetch_single') {
-        var id = request.body.id;
-
-        var query = `SELECT * FROM shift_1 WHERE id = "${id}"`;
-
-        database.query(query, function (error, data) {
-
-            response.json(data[0]);
-
-        });
-    }
-
-    if (action == 'Edit') {
-        var id = request.body.id;
-
-        var route_no = request.body.route_no;
-
-        var bus_no = request.body.bus_no;
-
-        var address = request.body.address;
-
-        var query = `
-		UPDATE shift_1
-		SET route_no = "${route_no}", 
-		bus_no = "${bus_no}", 
-		age = "${address}" 
-		WHERE id = "${id}"
-		`;
-
-        database.query(query, function (error, data) {
-            response.json({
-                message: 'Data Edited'
-            });
-        });
-    }
-
-    if (action == 'delete') {
-        var id = request.body.id;
-
-        var query = `DELETE FROM shift_1 WHERE id = "${id}"`;
-
-        database.query(query, function (error, data) {
-
-            response.json({
-                message: 'Data Deleted'
-            });
-
-        });
-    }
-
-});
 
 //admin shift2
 router.get("/admin_shift2.ejs", (req, res) => {
